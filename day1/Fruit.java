0b6785214ae0c8a241cff1f67d68d7c84ac3948c
lianallm <liana.masangkay88@gmail.com>
2021-01-01 22:33:50 -0500

    creating Vegetable and Fruit objects

--- day1/Fruit.java
+++ day1/Fruit.java
@@ -1,17 +1,18 @@
-// CLASSES AND OBJECTS PRACTICE!
-//goal: to use mult
+
 public class Fruit{
 
   //instance variables
   private String color;
   private double price;
   private double weight;
+  private int quantity;
 
   //constructor
-  public Fruit(String c, double p, double w){
+  public Fruit(String c, double p, double w, int q){
     this.color = c;
     this.price = p;
     this.weight = w; //in ounces!
+    this.quantity = q;
   }
 
   //accessor methods
@@ -28,16 +29,23 @@ public class Fruit{
     return weight;
   }
 
-  Fruit apple = new Fruit("red", 0.20, 5);
-  Fruit pear = new Fruit("green", 0.40, 7);
-  Fruit pineapple = new Fruit("yellow", 2.75 , 32);
-  Fruit banana = new Fruit("yellow", 0.60, 4);
-  Fruit mango = new Fruit("orange", 1.30, 6);
-  Fruit strawberry = new Fruit("red", 0.10 , 0.4);
+  public double getQuantity(){
+    return quantity;
+  }
+
+  public double setQuantity(int quant){
+    this.quantity = quant;
+  }
 
 
   public static void main(String[] args){
 
+    Fruit apple = new Fruit("red", 0.20, 5);
+    Fruit pear = new Fruit("green", 0.40, 7);
+    Fruit pineapple = new Fruit("yellow", 2.75 , 32);
+    Fruit banana = new Fruit("yellow", 0.60, 4);
+    Fruit mango = new Fruit("orange", 1.30, 6);
+    Fruit strawberry = new Fruit("red", 0.10 , 0.4);
 
 
   }
